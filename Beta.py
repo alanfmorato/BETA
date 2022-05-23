184b706b4baf9155dcdd8ccc62b796f516b69df7
tobiassousa <100934496+tobiassousa@users.noreply.github.com>
2022-05-23 20:57:11 -0300

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -14,7 +14,6 @@ import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
-
 while True:
     audio = sr.Recognizer()
     maquina = pyttsx3.init()
@@ -235,5 +234,28 @@ while True:
 
             maquina.say('Foram pesquisados 10 links que estão salvos em pdf em sua pasta downloads')
             maquina.runAndWait()
+        elif 'clima' in comando:
+            audio = sr.Recognizer()
+            maquina = pyttsx3.init()
+            with sr.Microphone() as source:
+                maquina.say("Informe a cidade da qual deseja descobrir a temperatura. ")
+                maquina.runAndWait()
+                print('ouvindo...')
+                voz = audio.listen(source)
+                cidade = audio.recognize_google(voz, language='pt-BR')
+                print(cidade)
+
+
+                API_KEY = "00a7922cfcccb7df823f10e7014e2e42"
+                link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
+
+                requisicao = requests.get(link)
+                requisicao_dic = requisicao.json()  # Faz a requisição
+                descricao = requisicao_dic['weather'][0]['description']  # Puxa a descrição de como esta o clima
+                temperatura = requisicao_dic['main']['temp'] - 273.15  # Puxa a temperatura atual e faz a conversão
+                maquina = pyttsx3.init()
+                maquina.say(f'Em {cidade} o céu está {descricao} e está {temperatura:.0f}ºC hoje')
+                maquina.runAndWait()
+
 
     comando_voz_usuario()
